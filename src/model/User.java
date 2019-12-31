d2906d3383a60bcedc5941cb6d434fbfa0016251
Diego Dominguez <d.dominguez.glez1111@gmail.com>
2019-12-31 23:30:37 +0100

    license notices added

--- src/model/User.java
+++ src/model/User.java
@@ -1,3 +1,21 @@
+/*
+ *
+ *  Copyright (c) 2019 Diego Dominguez Gonzalez
+ *
+ *	This file is part of JEEShop.
+ *
+ *  JEEShop is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or any later version.
+ *
+ *	JEEShop is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with JEEShop. If not, see <https://www.gnu.org/licenses/>.
+ */
 package model;
 /**
  * 
